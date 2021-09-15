3deffba2f3a80b079b018bb6c9605fb5a6e8844b
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-09-15 09:26:30 +0200

    Add a boilerplate for a 'circle' program
    The program must calculate the perimeter and the square of the circle,
    given the radius.

---
+++ circle/circle.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+
+Sample input/output:
+
+The program prints the perimeter and the square of a circle given the radius.
+Enter the radius: 8
+Perimeter: 50.27, square: 201.06
+
+*/
+func main() {
+	fmt.Println("The program prints the perimeter and the square of a circle given the radius.")
+	// ++ Your code here! ++
+	//
+	// Hint: Use math.Pi constant (https://pkg.go.dev/math#pkg-constants) to obtain π value.
+}
---
+++ circle/go.mod
@@ -0,0 +1,3 @@
+module github.com/prog-1/conversions/circle
+
+go 1.17
